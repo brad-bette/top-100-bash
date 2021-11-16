9c4fea57f0898d9b37ec021045289bbd8ecd62b4
Brad Bettencourt <77643576+brad-bette@users.noreply.github.com>
2021-11-15 16:38:06 -0800

    Add files via upload

---
+++ top-100.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+banner() {
+echo "ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+CiBffCAgICBffCAgICAgICAgICAgICAgX3wgICAgICAgICAgICBffCAgICBffCAgICAgIF98ICAg
+IAogX3wgICAgX3wgICAgX3xffCAgICBffF98X3xffCAgICAgIF98X3wgIF98ICBffCAgX3wgIF98
+ICAKIF98X3xffF98ICBffCAgICBffCAgICBffCAgICAgICAgICAgIF98ICBffCAgX3wgIF98ICBf
+fCAgCiBffCAgICBffCAgX3wgICAgX3wgICAgX3wgICAgICAgICAgICBffCAgX3wgIF98ICBffCAg
+X3wgIAogX3wgICAgX3wgICAgX3xffCAgICAgICAgX3xffCAgICAgICAgX3wgICAgX3wgICAgICBf
+fCAgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+ICAgIAo= " | base64 -d 2>/dev/null 
+}
+
+banner2() {
+echo "CiAgICAgZEJQIGRCUCBkQkJCQlAgZEJCQkJCQlAgICAgZEJCQmIgIGRCQkJCUCBkQkJCQlAKICAg
+ICAgICAgICAgZEJQLkJQICAgICAgICAgICAgICAgICAgICBkQicuQlAgZEInLkJQIAogICBkQkJC
+QkJQIGRCUC5CUCAgICBkQlAgICAgICAgICBkQlAgZEI6LkJQIGRCOi5CUCAgCiAgZEJQIGRCUCBk
+QlAuQlAgICAgZEJQICAgICAgICAgZEJQIGRCJy5CUCBkQicuQlAgICAKIGRCUCBkQlAgZEJCQkJQ
+ICAgIGRCUCAgICAgICAgIGRCUCBkQkJCQlAgZEJCQkJQICAgIAogICAgICAgICAgICAgICAgICAg
+ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCg==" | base64 -d 2>/dev/null
+}
+
+banner3() {
+echo "4paI4paI4pWXICDilojilojilZcg4paI4paI4paI4paI4paI4paI4pWXIOKWiOKWiOKWiOKWiOKW
+iOKWiOKWiOKWiOKVlyAgICAg4paI4paI4pWXIOKWiOKWiOKWiOKWiOKWiOKWiOKVlyAg4paI4paI
+4paI4paI4paI4paI4pWXIArilojilojilZEgIOKWiOKWiOKVkeKWiOKWiOKVlOKVkOKVkOKVkOKW
+iOKWiOKVl+KVmuKVkOKVkOKWiOKWiOKVlOKVkOKVkOKVnSAgICDilojilojilojilZHilojiloji
+lZTilZDilojilojilojilojilZfilojilojilZTilZDilojilojilojilojilZcK4paI4paI4paI
+4paI4paI4paI4paI4pWR4paI4paI4pWRICAg4paI4paI4pWRICAg4paI4paI4pWRICAgICAgIOKV
+muKWiOKWiOKVkeKWiOKWiOKVkeKWiOKWiOKVlOKWiOKWiOKVkeKWiOKWiOKVkeKWiOKWiOKVlOKW
+iOKWiOKVkQrilojilojilZTilZDilZDilojilojilZHilojilojilZEgICDilojilojilZEgICDi
+lojilojilZEgICAgICAgIOKWiOKWiOKVkeKWiOKWiOKWiOKWiOKVlOKVneKWiOKWiOKVkeKWiOKW
+iOKWiOKWiOKVlOKVneKWiOKWiOKVkQrilojilojilZEgIOKWiOKWiOKVkeKVmuKWiOKWiOKWiOKW
+iOKWiOKWiOKVlOKVnSAgIOKWiOKWiOKVkSAgICAgICAg4paI4paI4pWR4pWa4paI4paI4paI4paI
+4paI4paI4pWU4pWd4pWa4paI4paI4paI4paI4paI4paI4pWU4pWdCuKVmuKVkOKVnSAg4pWa4pWQ
+4pWdIOKVmuKVkOKVkOKVkOKVkOKVkOKVnSAgICDilZrilZDilZ0gICAgICAgIOKVmuKVkOKVnSDi
+lZrilZDilZDilZDilZDilZDilZ0gIOKVmuKVkOKVkOKVkOKVkOKVkOKVnSAK" | base64 -d 2>/dev/null
+}
+
+banner4() {
+echo "CiAoJy0uIC4tLiAgICAgICAgICAgICAgLi0nKSBfICAgICAgICAgICAgICAgICAgICAgICAgICAg
+ICAgICAgICAgCiggT08gKSAgLyAgICAgICAgICAgICAoICBPTykgKSAgICAgICAgICAgICAgICAg
+ICAgICAgICAgICAgICAgICAgCiwtLS4gLC0tLiAuLScpLC0tLS0tLiAvICAgICAnLl8gICAgICAg
+IC4tLS0uICAuLS0tLS4gICAgLi0tLS0uICAgCnwgIHwgfCAgfCggT08nICAuLS4gICd8Jy0tLi4u
+X18pICAgICAgL18gICB8IC8gIC4uICBcICAvICAuLiAgXCAgCnwgICAufCAgfC8gICB8ICB8IHwg
+IHwnLS0uICAuLS0nICAgICAgIHwgICB8LiAgLyAgXCAgLi4gIC8gIFwgIC4gCnwgICAgICAgfFxf
+KSB8ICB8XHwgIHwgICB8ICB8ICAgICAgICAgIHwgICB8fCAgfCAgJyAgfHwgIHwgICcgIHwgCnwg
+IC4tLiAgfCAgXCB8ICB8IHwgIHwgICB8ICB8ICAgICAgICAgIHwgICB8JyAgXCAgLyAgJycgIFwg
+IC8gICcgCnwgIHwgfCAgfCAgIGAnICAnLScgICcgICB8ICB8ICAgICAgICAgIHwgICB8IFwgIGAn
+ICAvICBcICBgJyAgLyAgCmAtLScgYC0tJyAgICAgYC0tLS0tJyAgICBgLS0nICAgICAgICAgIGAt
+LS0nICBgLS0tJycgICAgYC0tLScnICAgCg==" | base64 -d 2>/dev/null
+}
+
+# main
+
+# Random selector for the banner 1 - 4
+rand=`echo $RANDOM % 4 + 1 | bc`
+
+if [ $rand -eq 1 ] 
+then
+        banner
+elif [ $rand == 2 ]
+then
+	banner2
+elif [ $rand == 3 ]
+then
+	banner3
+else 
+	banner4
+fi
+
+wget --quiet https://www.billboard.com/charts/hot-100
+
+# Get the songs
+cat hot-100 | grep chart-element__information | grep song | sed -e 's/<[^>]*>//g' > songs
+
+# Get the artisit
+cat hot-100 | grep chart-element__information | grep artist | sed -e 's/<[^>]*>//g' > artist
+
+# get the total amoun of spaces for 1 line, this should be the same for each line
+a=`cat artist | head -n 1 | grep -o ' ' | wc -l`
+s=`cat songs | head -n 1 | grep -o ' ' | wc -l`
+
+# adjust this as nessary, if the song doesn't come out quite right
+s=$(echo $s - 1 | bc)
+
+i=1
+# Iterate through each artist in the field
+while [ $i -ne 101 ] 
+do 
+	cat artist | head -n $i | tail -n -1 | cut -b $a- >> art
+	i=`echo $i + 1 | bc`
+done
+
+j=1
+# Iterate through each song in the field
+while [ $j -ne 101 ] 
+do 
+	cat songs | head -n $j | tail -n -1 | cut -b $s- >> song
+	j=`echo $j + 1 | bc`
+done
+
+# rm the uneeded files
+rm songs artist
+
+# add numbers to the columns
+i=1 
+while [ $i -ne 101 ]
+do
+	echo $i >> numb  
+	i=`echo $i + 1 | bc`
+done
+
+# Remove unicode formating
+cat song | sed "s/&#039;/'/g" | sed "s/&amp;/and/g" > songs
+cat art | sed "s/&#039;/'/g" | sed "s/&amp;/and/g" > arts
+
+rm song art
+
+# combine each file to the collumns
+paste numb songs arts | column -s $'\t' -t > US-100.txt
+
+cat US-100.txt
+rm songs arts hot-100 numb
+
+
